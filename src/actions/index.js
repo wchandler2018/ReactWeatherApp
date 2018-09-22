78e7b520bc4686fb8ee1720aacb1681ddaa5fb15
william chandler <devine11>
2018-09-21 23:47:48 -0400

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_KEY = "038b2baa7d2346c2e39a1d4d3f9b1477";
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
+
+  console.log("Request:", request);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
